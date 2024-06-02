43acc0dca5adb26ab564c5c15d6636087ca4b882
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-03 02:01:24 +0500

    navbar component is started to be being built in navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import clsx from "clsx";
 import Layout from "./components/layout";
+import Navbar from "./components/layout/components/navbar";
 import Container from "./components/container";
 import "./index.css";
 import "./styles/global.scss";
 import "./App.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
