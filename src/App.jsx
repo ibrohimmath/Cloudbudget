1eb19601a5c4b2ac6a6cc0c7c80f206baaa69b5e
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-03 19:53:03 +0500

    added navbar within Layout component in layout branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import Navbar from "./components/layout/components/navbar";
-import * as typeAll from "./components/typography";
+import Layout from "./components/layout";
 import "./index.css";
 import "./styles/global.scss";
 import "./styles/_variables.scss";
@@ -10,7 +10,7 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <Navbar />
+      <Layout></Layout>
     </>
   );
 }
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Navbar from "./components/navbar";
 import cn from "./style.module.scss";
 
-export default function Layout() {
-  return <div>Layout</div>;
+export default function Layout({ children }) {
+  return (
+    <>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </>
+  );
 }
