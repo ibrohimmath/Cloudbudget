e530968c8c4658c598a036da2f694ff8c09749a0
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-03 15:09:53 +0500

    configured Navbar component without using Container component in navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/layout";
 import Navbar from "./components/layout/components/navbar";
 import "./index.css";
 import "./styles/global.scss";
+import "./styles/_variables.scss";
 import "./App.css";
 
 export default function App() {
--- src/components/layout/components/navbar/index.jsx
+++ src/components/layout/components/navbar/index.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import clsx from "clsx";
-import Container from "../../../container";
 import cloudSrc from "../../../../assets/icons/cloud.png";
 import cn from "./style.module.scss";
 
 export default function Navbar() {
   return (
     <>
-      <Container>
-        <header>
-          <nav>
-            <div className="logo">
-              <img src={cloudSrc} alt="Cloudbudge Logo" />
-            </div>
-          </nav>
-        </header>
-      </Container>
+      <header>
+        <nav>
+          <div className={clsx(cn["logo"])}>
+            <img src={cloudSrc} alt="Cloudbudge Logo" />
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
