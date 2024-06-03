13511f0fcc74d0f8e5ead61622746197ab6c476c
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-03 14:48:05 +0500

    changing container doesn't affect to navbar because navbar is fixed position in container branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import clsx from "clsx";
 import Layout from "./components/layout";
 import Navbar from "./components/layout/components/navbar";
-import Container from "./components/container";
 import "./index.css";
 import "./styles/global.scss";
 import "./App.css";
