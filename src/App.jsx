6dd81b2056339328b694d56b894b65896f666907
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-03 19:13:52 +0500

    responsive Navbar component is nearly completed in navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import clsx from "clsx";
 import Navbar from "./components/layout/components/navbar";
 import * as typeAll from "./components/typography";
-import * as btnAll from "./components/button";
 import "./index.css";
 import "./styles/global.scss";
 import "./styles/_variables.scss";
@@ -11,19 +10,7 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      <btnAll.Button
-        borderStyle={btnAll.ButtonBorderStyle.default}
-        borderSize={btnAll.ButtonBorderSize.sm}
-        borderColor={btnAll.ButtonBorderColor.white}
-        borderRadius={btnAll.ButtonRadius.md}
-        background={btnAll.ButtonBackground.primary}
-        padding={btnAll.ButtonPadding.md}
-      >
-        <typeAll.Type color={typeAll.TextColor.white}>
-          Lorem ipsum dolor sit amet
-        </typeAll.Type>
-      </btnAll.Button>
+      <Navbar />
     </>
   );
 }
